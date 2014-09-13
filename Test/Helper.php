3cf0214405b262a430c21c1168ccc5258aad9dc0
Gerd Katzenbeisser <gerd.katzenbeisser@gmail.com>
2014-09-13 20:57:25 +0200

    Embed BaseTest class directly instead of loading it from the EPS package

--- Test/Helper.php
+++ Test/Helper.php
@@ -1,3 +1,16 @@
 <?php
 
-require_once join(DS, array(EPS_BANK_TRANSFER_APP, 'Lib', 'EPS', 'tests', 'unit', 'at', 'externet', 'eps_bank_transfer', 'BaseTest.php'));
+namespace at\externet\eps_bank_transfer;
+
+abstract class BaseTest extends \PHPUnit_Framework_TestCase
+{
+    public static function GetEpsData($filename)
+    {
+        return file_get_contents(self::GetEpsDataPath($filename));
+    }
+
+    public static function GetEpsDataPath($filename)
+    {
+        return __DIR__ . DIRECTORY_SEPARATOR . 'EpsData' . DIRECTORY_SEPARATOR . $filename;
+    }
+}
