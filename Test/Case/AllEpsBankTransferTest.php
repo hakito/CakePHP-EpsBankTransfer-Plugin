2081404e610fa3ce14fb612dcf3815a6856dc7e5
Gerd Katzenbeisser <gerd.katzenbeisser@gmail.com>
2014-09-13 20:54:17 +0200

    Add file for all tests

---
+++ Test/Case/AllEpsBankTransferTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * All  plugin tests
+ */
+class AllEpsBankTransferTest extends CakeTestCase {
+
+/**
+ * Suite define the tests for this plugin
+ *
+ * @return void
+ */
+	public static function suite() {
+		$suite = new CakeTestSuite('All test');
+
+		$path = CakePlugin::path('EpsBankTransfer') . 'Test' . DS . 'Case' . DS;
+		$suite->addTestDirectoryRecursive($path);
+
+		return $suite;
+	}
+
+}
