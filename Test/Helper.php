7529ad0d7537b28e189f491347fad922e88a7c41
Gerd Katzenbeisser <gerd.katzenbeisser@gmail.com>
2014-09-14 18:35:01 +0200

    Set path in Helper.php for travis.ci builds

--- Test/Helper.php
+++ Test/Helper.php
@@ -5,7 +5,7 @@ if (file_exists($baseTestFile))
     require_once $baseTestFile;
 
 // for travic-ci tests
-$baseTestFile = join(DS, array(dirname(__DIR__), 'vendor', 'hakito', 'php-stuzza-eps-banktransfer', 'tests', 'unit', 'at', 'externet', 'eps_bank_transfer', 'BaseTest.php'));
+$baseTestFile = join(DS, array(APP, 'vendor', 'hakito', 'php-stuzza-eps-banktransfer', 'tests', 'unit', 'at', 'externet', 'eps_bank_transfer', 'BaseTest.php'));
 if (file_exists($baseTestFile))
     require_once $baseTestFile;
 unset($baseTestFile);
