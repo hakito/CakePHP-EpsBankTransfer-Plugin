de9b25fc45e5735edfde3d552bff985f5434068a
Gerd Katzenbeisser <gerd.katzenbeisser@gmail.com>
2014-09-14 17:14:10 +0200

    Set composer compatible path for LibTest

--- Test/Case/LibTest.php
+++ Test/Case/LibTest.php
@@ -1,10 +1,10 @@
 <?php
-require_once EPS_BANK_TRANSFER_APP . 'Test' . DS . 'Helper.php';
+
 class LibTest extends CakeTestCase
 {
     public static function suite() {
         $suite = new CakeTestSuite('Lib tests');
-        $suite->addTestDirectoryRecursive(join(DS, array(EPS_BANK_TRANSFER_APP,'Lib','EPS','tests')));
+        $suite->addTestDirectoryRecursive(join(DS, array(APP, 'Vendor', 'hakito', 'php-stuzza-eps-banktransfer', 'tests')));
         return $suite;
     }
 }
