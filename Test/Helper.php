36181a3b7c6a5442385080bbd4f15047e0cdf5e4
Gerd Katzenbeisser <gerd.katzenbeisser@gmail.com>
2014-09-14 19:36:18 +0200

    Load BaseTest class in Helper.php from 'Vendor' or 'vendor' directory

--- Test/Helper.php
+++ Test/Helper.php
@@ -1,11 +1,14 @@
 <?php
 
 $baseTestFile = join(DS, array(APP, 'Vendor', 'hakito', 'php-stuzza-eps-banktransfer', 'tests', 'unit', 'at', 'externet', 'eps_bank_transfer', 'BaseTest.php'));
+if (!file_exists($baseTestFile))
+{
+    // for travic-ci tests
+    $baseTestFile = join(DS, array(APP, 'vendor', 'hakito', 'php-stuzza-eps-banktransfer', 'tests', 'unit', 'at', 'externet', 'eps_bank_transfer', 'BaseTest.php'));
+    require_once $baseTestFile;
+}
+
 if (file_exists($baseTestFile))
     require_once $baseTestFile;
 
-// for travic-ci tests
-$baseTestFile = join(DS, array(APP, 'vendor', 'hakito', 'php-stuzza-eps-banktransfer', 'tests', 'unit', 'at', 'externet', 'eps_bank_transfer', 'BaseTest.php'));
-if (file_exists($baseTestFile))
-    require_once $baseTestFile;
 unset($baseTestFile);
